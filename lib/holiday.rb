773ec3c43a94afcae11891474746213d2e74c8d1
Mark Crowder <crowdered@gmail.com>
2019-04-02 15:09:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,13 +46,12 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holidays_with_supplies|
-    holidays_with_supplies.each do |holiday, supplies|
-      if supplies.include?("BBQ") 
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      holiday if supplies.include?("BBQ") 
+    end 
       end.flatten.compact
     end
-  end
-end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
