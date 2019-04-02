c8109e3767c3d5ec01f83ffba707c5c91f2fcd60
Mark Crowder <crowdered@gmail.com>
2019-04-02 15:07:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays_with_supplies|
     holidays_with_supplies.each do |holiday, supplies|
       if supplies.include?("BBQ") 
-        return holiday
+        
       end
     end
   end
