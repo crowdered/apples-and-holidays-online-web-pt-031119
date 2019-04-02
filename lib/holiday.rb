0d028b7497ba9530a98fab696e5184e3b301a524
Mark Crowder <crowdered@gmail.com>
2019-04-02 15:08:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays_with_supplies|
     holidays_with_supplies.each do |holiday, supplies|
       if supplies.include?("BBQ") 
-      end
+      end.flatten.compact
     end
   end
 end
